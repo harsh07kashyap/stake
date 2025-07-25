e9481ff83db23ec2348d4149c6004c1ab6827f0e
harsh07kashyap <kashyap7.harsh@gmail.com>
2025-07-25 15:58:23 +0530

    frontend and backend folder added

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import {Home} from "./pages/Game";
+import {Game} from "./pages/Game";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path="/" element={<Home />} /> */}
+        <Route path="/" element={<Game />} />
+        
+      </Routes>
+    </BrowserRouter>
+  )
+}
+export default App;
